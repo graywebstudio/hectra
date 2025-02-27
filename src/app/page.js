2d152eef60e5c46d2688165513b21fdbe64b1a28
graywebstudio <renknockranjith33@gmail.com>
2025-02-27 17:11:32 +0530

    Update page.js
    another redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-import HomePage from "./home/page";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+   const router = useRouter();
+  
+  useEffect(() => {
+    router.replace("/home");
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-       <HomePage/>
+       
     </div>
   );
 }
